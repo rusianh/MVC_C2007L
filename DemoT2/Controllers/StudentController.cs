e5ba3afcd36efa9d8c79ae6aece230c0dd40474c
Cris Dev <vva1994@gmail.com>
2022-05-04 22:15:35 +0700

    Add project files.

---
+++ DemoT2/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoT2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewData["Name"] = "Cris";
+            ViewData["Age"] = "18";
+            ViewData["Adress"] = "Hanoi";
+            return View("ShowData");
+        }
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
