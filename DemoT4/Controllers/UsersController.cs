fce56f43c7825563fc9a9cfea0107477f9eaf0dd
Cris Dev <vva1994@gmail.com>
2022-05-18 15:47:12 +0700

    add

---
+++ DemoT4/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using DemoT4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoT4.Controllers
+{
+    public class UsersController : Controller
+    {
+        public static List<User> list = new List<User>()
+        {
+            new User(){ Name = "Huy", Email="huy@",Id=1, Password="huy123"},
+            new User(){ Name = "Phuong", Email = "Phuong@yahoo.com", Id=2, Password="phuong123"}
+        };
+        // GET: Users
+        public ActionResult Index()
+        {
+
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User _user)
+        {
+            list.Add(_user);
+            //return View("index",list);
+            return RedirectToAction("Index",list);
+        }
+    }
+}
---
+++ DemoT4/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoT4.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string  Email { get; set; }
+        [Required(ErrorMessage = "Password is Required.")]
+        public string Password { get; set; }
+
+    }
+}
